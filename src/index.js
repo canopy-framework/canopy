f4a9561a8cdfef0fda9b310e2423d4d9cc898c1c
Alex Riviere <alexanderm.riviere@gmail.com>
2023-07-10 19:43:42 -0400

    added a destroy command with no-op functionality

--- src/index.js
+++ src/index.js
@@ -9,10 +9,18 @@ const deploy = () => {
   console.log("Deploying AWS infrastructure necessary to run Canopy");
 }
 
+const destroy = () => {
+  console.log("Destroying Canopy AWS infrastructure");
+}
+
 program
   .command('deploy')
   .description("Deploy Canopy Infrastructure to AWS")
   .action(deploy);
 
+program
+  .command('destroy')
+  .description("Destroy Canopy Infrastructure on AWS")
+  .action(destroy);
 
 program.parse(process.argv);
