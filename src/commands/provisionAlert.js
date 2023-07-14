1705f259cc05b0393fb828ecc3954443f2e735b5
Alex Riviere <alexanderm.riviere@gmail.com>
2023-07-14 10:28:36 -0400

    new alerts added to constants folder, old alert time updated to 5 mins ago

--- src/commands/provisionAlert.js
+++ src/commands/provisionAlert.js
@@ -2,7 +2,6 @@ const inquirer = require('inquirer');
 const axios = require('axios');
 const user = "admin";
 const password = "admin";
-// admin:admin@Localhost:3000
 const scheme = "http://"
 const host = "localhost";
 const port = 3000;
@@ -11,18 +10,7 @@ const getDataSourceUIDPath = "/api/datasources/name/ClickHouse";
 const getFoldersPath = "/api/folders";
 const ruleGroup = "pre-configured"; 
 const folderName = "Pre-Configured Alerts";
-let alert1Template = {
-  "annotations":{"__dashboardUid__":"d26bb8ae-aea9-4b85-bc17-5b81054a0217","__panelId__":"3"},
-  "condition":"C",
-  "data":[{"refId":"A","queryType":"sql","relativeTimeRange":{"from":3667837,"to":39038},"datasourceUid":"","model":{"builderOptions":{"0":"T","1":"h","10":"i","11":"s","12":" ","13":"n","14":"o","15":"t","16":" ","17":"a","18":" ","19":"s","2":"e","20":"e","21":"l","22":"e","23":"c","24":"t","25":" ","26":"s","27":"t","28":"a","29":"t","3":" ","30":"e","31":"m","32":"e","33":"n","34":"t","35":".","4":"q","5":"u","6":"e","7":"r","8":"y","9":" ","fields":[],"filters":[{"condition":"AND","filterType":"custom","key":"timestamp","operator":"WITH IN DASHBOARD TIME RANGE","restrictToFields":[{"label":"timestamp","name":"timestamp","picklistValues":[],"type":"DateTime"}],"type":"datetime"}],"limit":100,"metrics":[],"mode":"trend","orderBy":[],"table":"realtime_logs","timeField":"timestamp","timeFieldType":"DateTime"},"datasource":{"type":"grafana-clickhouse-datasource","uid":""},"intervalMs":1000,"maxDataPoints":43200,"meta":{"builderOptions":{"0":"T","1":"h","10":"i","11":"s","12":" ","13":"n","14":"o","15":"t","16":" ","17":"a","18":" ","19":"s","2":"e","20":"e","21":"l","22":"e","23":"c","24":"t","25":" ","26":"s","27":"t","28":"a","29":"t","3":" ","30":"e","31":"m","32":"e","33":"n","34":"t","35":".","4":"q","5":"u","6":"e","7":"r","8":"y","9":" ","fields":[],"filters":[{"condition":"AND","filterType":"custom","key":"timestamp","operator":"WITH IN DASHBOARD TIME RANGE","restrictToFields":[{"label":"timestamp","name":"timestamp","picklistValues":[],"type":"DateTime"}],"type":"datetime"}],"limit":100,"metrics":[],"mode":"trend","orderBy":[],"table":"realtime_logs","timeField":"timestamp","timeFieldType":"DateTime"}},"queryType":"sql","rawSql":"SELECT toStartOfInterval(timestamp, INTERVAL (($__toTime - $__fromTime) / 20) SECOND ) AS d,\nquantile(0.95)(time_taken) AS `95 Percentile`\nFROM \"realtime_logs\"\nWHERE timestamp  \u003e= $__fromTime AND timestamp \u003c= $__toTime\nGROUP BY d ORDER BY d ASC","refId":"A"}},{"refId":"B","relativeTimeRange":{"from":3667837,"to":39038},"datasourceUid":"__expr__","model":{"conditions":[{"evaluator":{"params":[],"type":"gt"},"operator":{"type":"and"},"query":{"params":["B"]},"reducer":{"params":[],"type":"last"},"type":"query"}],"datasource":{"type":"__expr__","uid":"__expr__"},"expression":"A","hide":false,"intervalMs":1000,"maxDataPoints":43200,"reducer":"last","refId":"B","type":"reduce"}},{"refId":"C","relativeTimeRange":{"from":3667837,"to":39038},"datasourceUid":"__expr__","model":{"conditions":[{"evaluator":{"params":[0.05],"type":"gt"},"operator":{"type":"and"},"query":{"params":["C"]},"reducer":{"params":[],"type":"last"},"type":"query"}],"datasource":{"type":"__expr__","uid":"__expr__"},"expression":"B","hide":false,"intervalMs":1000,"maxDataPoints":43200,"refId":"C","type":"threshold"}}],
-  "execErrState":"Error",
-  "folderUID":"",
-  "for":"5m",
-  "noDataState":"NoData",
-  "orgID":1,
-  "ruleGroup":"",
-  "title":"Time Taken > 600ms",
-}
+const { alertTemplates } = require('../constants/alertTemplates');
 
 /*
 Here, you can either include options, which is probably how we will use this command from the Dashboard. If you don't include options, you are show a prompt where you can choose which alerts you want to set up. 
@@ -130,7 +118,7 @@ const provisionAlert = async (options) => {
         ]
       },
     ]);
-    // standardize `alerts` so that it looks the same as if the options were used, an object with 
+    // standardize `alerts` so that it looks the same as if the options were used
     alertsSelected.alerts.forEach(alertName => alerts[alertName] = true);
   }
   
