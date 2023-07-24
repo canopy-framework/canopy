a255f2f66397472de3eaadfcdb5cde20b06d6ffb
Med <elmanioui.mehdi@gmail.com>
2023-07-24 01:09:17 +0000

    added tests for user input validation helpers
    Co-authored-by: Michael Shyer <mshyer@outlook.com>
    Co-authored-by: Jason Ng <jasonscoat@yahoo.com>
    Co-authored-by: Alex Riviere <alexanderm.riviere@gmail.com>

--- src/utils/user-input-validation.js
+++ src/utils/user-input-validation.js
@@ -13,19 +13,19 @@ const isValidCloudFrontDistributionID = (distributionID) => {
 const isValidAWSAccessKeyID = (accessKeyID) => {
   const awsAccessKeyIDRegex = /^[A-Z0-9]{20}$/;
 
-  return awsAccessKeyIDRegex.test(accessKeyID) || "Invalid AWS Access Key Id";
+  return awsAccessKeyIDRegex.test(accessKeyID) || "Invalid AWS Access Key Id!";
 };
 
 const isValidAWSSecretAccessKey = (secretAccessKey) => {
   const awsSecretAccessKeyRegex = /^[A-Za-z0-9/+=]{40}$/;
   
-  return awsSecretAccessKeyRegex.test(secretAccessKey) || "Invalid AWS Secret Access Key";
+  return awsSecretAccessKeyRegex.test(secretAccessKey) || "Invalid AWS Secret Access Key!";
 };
 
 const isValidHttpEndpoint = (endpoint) => {
   const regex = /^https?:\/\/.+/;
 
-  return regex.test(endpoint) || "Invalid HTTP endpoint";
+  return regex.test(endpoint) || "Invalid HTTP endpoint!";
 };
 
 module.exports = {
