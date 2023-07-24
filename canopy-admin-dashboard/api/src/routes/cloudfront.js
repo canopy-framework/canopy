a52321cbf7b333531cafe6aab98a840488303ab1
Alex Riviere <alexanderm.riviere@gmail.com>
2023-07-24 10:01:47 -0400

    cloudfront route for dashboard api serves multiple distributions
    Co-authored-by: Michael Shyer <mshyer@outlook.com>
    Co-authored-by: Jason Ng <jasonscoat@yahoo.com>
    Co-authored-by: El Mehdi El Manioui <elmanioui.mehdi@gmail.com>

--- canopy-admin-dashboard/api/src/routes/cloudfront.js
+++ canopy-admin-dashboard/api/src/routes/cloudfront.js
@@ -6,17 +6,25 @@ const router = Router();
 
 const getCloudFrontInfo = () => {
   const awsConfigData = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', '..', '..', 'aws-config.json')));
+  let distributionData = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', '..', '..', 'cloudfront-distributions.json'))).map((distObject) => distObject.distributionId);
+  distributionData.unshift(awsConfigData.distributionId);
 
-  const info = {};
+  
+  let info = [];
+
   try {
-    info['distributionId'] = awsConfigData.distributionId;
-    info['region'] = awsConfigData.region;
-    info['deployed'] = String(awsConfigData['deployed']);
+    distributionData.forEach((id, index) => {
+      info[index] = {};
+      info[index]['distributionId'] = id;
+      info[index]['region'] = awsConfigData.region;
+      info[index]['deployed'] = String(!!awsConfigData['deployed']);
+      
+    });
   } catch (err) {
     return { error: 'Error fetching CloudFront info ' + err };
   }
-  // return info; so now just make sure that info matches the format below
-  return [{distributionId: 'sdfsd', region: 'us-east-1', deployed: 'false'}, {distributionId: 'sdfsd', region: 'us-east-1', deployed: 'falsey babe'}, {distributionId: 'sdfsd', region: 'us-east-1', deployed: 'falsey babadfe'}, {distributionId: 'sdfsd', region: 'us-east-1', deployed: 'falsey boasdf'}];
+  return info; //so now just make sure that info matches the format below
+  // return [{distributionId: 'sdfsd', region: 'us-east-1', deployed: 'false'}, {distributionId: 'sdfsd', region: 'us-east-1', deployed: 'falsey babe'}, {distributionId: 'sdfsd', region: 'us-east-1', deployed: 'falsey babadfe'}, {distributionId: 'sdfsd', region: 'us-east-1', deployed: 'falsey boasdf'}];
 };
 
 router.get('/info', (req, res) => {
