9730814fc39981302ed437c860eeeee78aa32f4c
Med <elmanioui.mehdi@gmail.com>
2023-07-28 02:17:11 +0000

    refactored bash script for dynamic execution from another file
    Co-authored-by: Michael Shyer <mshyer@outlook.com>
    Co-authored-by: Jason Ng <jasonscoat@yahoo.com>
    Co-authored-by: Alex Riviere <alexanderm.riviere@gmail.com>

--- canopy-admin-dashboard/api/setup_dashboard_db.sh
+++ canopy-admin-dashboard/api/setup_dashboard_db.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Get the absolute path of the script directory
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 check_command() {
   if ! command -v "$1" &> /dev/null; then
     echo "Error: $1 is not installed. Installing it now..."
-    sudo apt-get update > /dev/null 2>&1
-    sudo apt-get -y install "$1" > /dev/null 2>&1
+    sudo apt-get update
+    sudo apt-get -y install "$1" 
   fi
 }
 
 start_postgres_server() {
-  if ! sudo service postgresql status > /dev/null 2>&1; then
+  if ! sudo service postgresql status; then
     echo "Starting PostgreSQL server..."
     sudo service postgresql start
     sleep 5
@@ -26,21 +29,21 @@ echo "Downloading and installing PostgreSQL..."
 RELEASE=$(lsb_release -cs)
 echo "deb http://apt.postgresql.org/pub/repos/apt/ $RELEASE-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update > /dev/null 2>&1
-sudo apt-get -y install postgresql > /dev/null 2>&1
+sudo apt-get update
+sudo apt-get -y install postgresql 
 
 start_postgres_server
 
 # Create dashboard database
 DB_NAME="dashboard_storage"
 echo "Creating the database $DB_NAME"
-sudo -u postgres psql -c "CREATE DATABASE $DB_NAME;" 2>/dev/null
+sudo -u postgres psql -c "CREATE DATABASE $DB_NAME;" 
 
-
-SQL_FILE="dashboard_schema.sql"
+# Use the absolute path for dashboard_schema.sql
+SQL_FILE="$SCRIPT_DIR/dashboard_schema.sql"
 
 echo "Creating table cdn_distributions..."
-sudo -u postgres psql $DB_NAME -f "$SQL_FILE"
+sudo -u postgres psql $DB_NAME -f "$SQL_FILE" 
 
 echo "Database and table created successfully."
 
